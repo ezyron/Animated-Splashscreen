b1b416886b33ef362bc96880e3295c89b0f37cb2
Kieron Norfield <kieron@freshstripes.com>
2012-07-26 12:12:38 +0100

    Blank project

---
+++ splashscreen/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  splashscreen
+//
+//  Created by Kieron Norfield on 26/07/2012.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ splashscreen/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  splashscreen
+//
+//  Created by Kieron Norfield on 26/07/2012.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
